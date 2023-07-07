caa8a7e1adefe93ad3effd9897f241cb540dd832
Aditykumari <127773486+Aditykumari@users.noreply.github.com>
2023-07-07 10:54:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("myButton").addEventListener("click", function() {
+    window.open("http://127.0.0.1:5500/restaurants.html", "_blank");
+  });
+  //AIzaSyCIR0w4AbpL-dLfzZzK5emuH83xPovoO1k
+  
+  
